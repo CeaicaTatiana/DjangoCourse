59e8116b65977827b7ae67aa822e74d7ca09ab6d
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-02-22 19:50:15 +0200

    if-else if-else statement

--- Java Script/control_flow.js
+++ Java Script/control_flow.js
@@ -1,5 +1,5 @@
 var hot = false;
-var temp = 50;
+var temp = 10;
 
 
 // if (temp>80){
@@ -8,8 +8,18 @@ var temp = 50;
 //
 // console.log(hot);
 
-if (temp>80){
+// if (temp>80){
+//   console.log("Hot Outside!");
+// } else{
+//   console.log("It's not very hot today!")
+// }
+
+if (temp>80) {
   console.log("Hot Outside!");
+} else if(temp <= 80 && temp >=50){
+  console.log("Average temp Outside");
+} else if (temp < 50 && temp >=32){
+  console.log("It's pretty cold out!");
 } else{
-  console.log("It's not very hot today!")
+  console.log("It is very cold out");
 }
