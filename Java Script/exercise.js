04816eda72c704b6837ecafcdd979b4c1e6c2e29
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-02-22 18:22:08 +0200

    accepted a number input in pounds, converted it in kg and  reported an alert

---
+++ Java Script/exercise.js
@@ -0,0 +1,5 @@
+alert("Welcome to my conventer!")
+var pounds = prompt("How many pound would you like to convert?")
+var kg = pounds * 0.454
+alert(pounds + " pounds" + " are " + kg + " kg" )
+console.log("Conversion Completed")
