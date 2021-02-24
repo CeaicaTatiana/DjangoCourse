9e46c5915bc433c2df4fbd02845c26d3135fb521
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-02-24 12:02:38 +0200

    Used a For Loop and a While Loop to print the word "hello" 5 times

---
+++ Java Script/loop_exercises.js
@@ -0,0 +1,18 @@
+//Use a For Loop and a While Loop to print the word "hello" 5 times
+
+//While Loop
+
+var n = 1;
+
+while (n<=5) {
+  console.log("hello");
+  n++;
+}
+
+
+//For Loop
+
+
+for (var i = 1; i <=5; i++) {
+  console.log("hello");
+}
