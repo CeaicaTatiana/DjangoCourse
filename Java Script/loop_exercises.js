3b082d38bcaecae88f9bb9264a56890f913d4629
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-02-24 12:11:39 +0200

    Use For and While Loops to print out all the odd numbers from 1 to 25

--- Java Script/loop_exercises.js
+++ Java Script/loop_exercises.js
@@ -16,3 +16,25 @@ while (n<=5) {
 for (var i = 1; i <=5; i++) {
   console.log("hello");
 }
+
+
+//Use Loops to print out all the odd numbers from 1 to 25
+
+//While Loop
+
+var x = 1;
+
+while (x<=25) {
+  if (x%2!==0) {
+    console.log(x);
+  }
+  x++;
+}
+
+//For Loop
+
+for (var i = 1; i<=25; i++) {
+  if (i%2!==0) {
+    console.log(i);
+  }
+}
