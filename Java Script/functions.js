18a45af1c69003f49778e9ebeb8fa71612af5b24
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-02-25 13:10:12 +0200

    global scope

--- Java Script/functions.js
+++ Java Script/functions.js
@@ -22,6 +22,21 @@ function formal (name="Sam", title="Sir"){
 }
 
 function timesFive(numInput) {
+  //local scope to the function
   var result = numInput * 5
   return result
 }
+
+//Global scope
+
+var v = " GLOBAL V"
+var stuff = " GLOBAL STUFF"
+
+function fun(stuff) {
+  console.log(v);
+  stuff = "Reassign stuff inside func"
+  console.log(stuff);
+}
+
+fun()
+console.log(stuff); 
