5044ac8ba92a6b404284c70750ea6e8e5ec14482
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-02-25 17:03:31 +0200

    function sum problem

--- Java Script/function_exercises.js
+++ Java Script/function_exercises.js
@@ -34,4 +34,27 @@ function stringTimes(str,n) {
     returnstr += str;
     i++
   }
+  return returnstr
+}
+
+//Given 3 numerical values, a,b,c, return their sum. However, if one of the value is 13, then it does not count,
+//towards the sum and values to its right.
+//If b is 13, then both  b and c do not count.
+
+//luckySum(1,2,3) - 6
+//luckySum(1,2,13) - 3
+//luckySum(1,13,3) - 1
+
+
+function luckySum(a,b,c) {
+  if (a == 13) {
+    return 0
+  } else if (b == 13) {
+    return a
+  } else if (c == 13) {
+    return a + b
+  } else {
+    return a+b+c
+  }
+
 }
