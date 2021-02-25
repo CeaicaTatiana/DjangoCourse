ff4319876c14f9dd2378d9c670327da3348b9789
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-02-25 12:07:32 +0200

    described the conditions

--- Java Script/Level_One_Project.js
+++ Java Script/Level_One_Project.js
@@ -2,7 +2,7 @@ var firstName = prompt("Hello and Welcome! Please enter your First Name:");
 var lastName = prompt("Enter your Last Name");
 var age = prompt("How old are you?");
 var height = prompt("How tall are you in cm?");
-var pet = prompt("What is the name of your pet?");
+var petName = prompt("What is the name of your pet?");
 alert("Thank you so much for the information!");
 
 
@@ -10,3 +10,32 @@ var nameCond = null;
 var ageCond = null;
 var heightCond = null;
 var petCond = null;
+
+//Name condition
+if (firstName[0] === lastName[0]) {
+  nameCond = true;
+} else {
+  nameCond = false;
+}
+//Age condition
+if (age > 20 && age <= 30) {
+  ageCond = true;
+} else {
+  ageCond = false;
+}
+
+//Height condition
+
+if (height <= 170) {
+  heightCond = true;
+} else {
+  heightCond = false;
+}
+
+// Pet condition
+
+if (petName[petName.length-1] === "y") {
+  petCond = true;
+} else {
+  petCond = false;
+}
