a28fe362400f0ab0c997821038063b2fd8d87535
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-02-25 12:45:17 +0200

    functions with default values

--- Java Script/functions.js
+++ Java Script/functions.js
@@ -5,7 +5,12 @@ function hello(){
 function helloYou(name){
   console.log("Hello "+ name);
 }
-
+//two parameters
 function addNum(num1,num2) {
   console.log(num1+num2);
+}
+//default values
+
+function helloSomeone(name="Frank") {
+  console.log("Hello "+name);
 }
