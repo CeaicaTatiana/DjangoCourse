81a619f79a4f2c660e591e643bcbb093cf7494c4
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-02-25 11:51:45 +0200

    set conditions

--- Java Script/Level_One_Project.js
+++ Java Script/Level_One_Project.js
@@ -1,6 +1,12 @@
-var firstName = prompt("Hello and Welcome! Please enter your First Name:")
-var lastName = prompt("Enter your Last Name")
-var age = prompt("How old are you?")
-var height = prompt("How tall are you in cm?")
-var pet = prompt("What is the name of your pet?")
-alert("Thank you so much for the information!")
+var firstName = prompt("Hello and Welcome! Please enter your First Name:");
+var lastName = prompt("Enter your Last Name");
+var age = prompt("How old are you?");
+var height = prompt("How tall are you in cm?");
+var pet = prompt("What is the name of your pet?");
+alert("Thank you so much for the information!");
+
+
+var nameCond = null;
+var ageCond = null;
+var heightCond = null;
+var petCond = null;
