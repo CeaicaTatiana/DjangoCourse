b77b5e3ddaa1e47de3545edef31063e82939bbc8
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-02-27 23:04:26 +0200

    document attributes

---
+++ DOM/iteraction.js
@@ -0,0 +1,11 @@
+document.getElementById("pickme") // grab element by ID
+
+document.getElementsByClassName("myul") // by class name
+
+document.getElementsByTagName('li') // by tag name
+
+document.querySelector("#pickme")
+
+document.querySelectorAll("li")
+
+document.querySelectorAll(".myul")
