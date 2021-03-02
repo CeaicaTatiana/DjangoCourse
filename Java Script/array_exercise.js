517c19891b3164247b5aed651ca633dad37fcaec
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-03-02 14:04:06 +0200

    created the functions for the tasks

--- Java Script/array_exercise.js
+++ Java Script/array_exercise.js
@@ -2,3 +2,19 @@ var roster = [];
 var start = prompt("Would you like to start the roster web app? y/n");
 var action = prompt("Please select an action: add, remove, display, or quit.");
 var name = prompt("What name would you like to add?");
+
+// create the functions for the tasks
+
+//ADD A NEW STUDENT
+
+function addNew(name) {
+  roster.push(name)
+}
+
+//REMOVE A STUDENT
+
+function remove() {
+  var remName = prompt("What name would you like to delete?");
+  var index = roster.indexOf(remName);
+  roster.splice(index,1)
+}
