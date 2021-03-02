aafdc7972ffdd3a07e547adf3db7b4bf3fb6824b
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-03-02 13:23:13 +0200

    added variables

---
+++ Java Script/array_exercise.js
@@ -0,0 +1,4 @@
+var roster = [];
+var start = prompt("Would you like to start the roster web app? y/n");
+var action = prompt("Please select an action: add, remove, display, or quit.");
+var name = prompt("What name would you like to add?");
