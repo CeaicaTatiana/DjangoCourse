2b1ed092cb9c22c13619ce7165549269edc150fa
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-03-02 14:17:14 +0200

    working version

--- Java Script/array_exercise.js
+++ Java Script/array_exercise.js
@@ -1,14 +1,14 @@
 var roster = [];
 var start = prompt("Would you like to start the roster web app? y/n");
-var action = prompt("Please select an action: add, remove, display, or quit.");
-var name = prompt("What name would you like to add?");
+var request = "empty"
 
 // create the functions for the tasks
 
 //ADD A NEW STUDENT
 
-function addNew(name) {
-  roster.push(name)
+function addNew(newName) {
+  var name = prompt("What name would you like to add?");
+  roster.push(newName)
 }
 
 //REMOVE A STUDENT
@@ -18,3 +18,22 @@ function remove() {
   var index = roster.indexOf(remName);
   roster.splice(index,1)
 }
+
+//DISPLAY ROSTER
+function display() {
+  console.log(roster);
+}
+//logic
+if (start === 'y') {
+  while (request !== "quit") {
+    request = prompt("Please select an action: add, remove, display, or quit.")
+    if (request === 'add') {
+      addNew();
+    }else if (request === 'remove') {
+      remove();
+    }else if (request === 'display') {
+      display();
+    }
+  }
+}
+alert("Thanks for using the Web App!")
