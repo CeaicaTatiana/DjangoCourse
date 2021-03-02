76316ceb4be1a0bba95f0909fb66b1c8df8ee1cd
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-03-02 18:18:06 +0200

    wrote a program that  creates an Alert in the browser

--- Java Script/object_exercise.js
+++ Java Script/object_exercise.js
@@ -1,10 +1,15 @@
 //Add a method nameLength that prints out the length of the employees to the console
+//write a program that will create an Alert in the browser of each of the objet's values for the key value pairs
+
 
 var employee = {
   name: "John Smith",
   job: "Programmer",
   age: 31,
+  report: function(){
+    alert("Name is" + this.name, "Job is" + this.job + "Age is" + this.age)
+  },
   nameLength: function(){
-    console.log(this.name.length);
+    console.log(this.name);
   }
 }
