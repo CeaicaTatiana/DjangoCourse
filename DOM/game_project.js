702ba79f6fc019d064cc45875dccf1c92c7ac789
Ceaica Tatiana <ceaica.tatiana@gmail.com>
2021-03-03 12:28:17 +0200

    created variables, clear board function and restart button

--- DOM/game_project.js
+++ DOM/game_project.js
@@ -1,12 +1,17 @@
 //Restart Game Button
-
+var restart = document.querySelector("#b");
 
 //Grab all the squares
-
+var squares = document.querySelectorAll('td');
 
 //Clear all the squeres
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = '';
+  }
+}
 
-
+restart.addEventListener('click',clearBoard);
 //Check the square marker
 
 
